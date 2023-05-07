86c111d3ff176d9e9097c8c8ebaf8464e3c1fd49
Vũ Đình Đô <102849264+vudinhdo@users.noreply.github.com>
2023-05-08 02:03:46 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,20 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    if(h<10){
+        h = "0"+h;
+    }
+    document.getElementById('h').innerHTML = h;
+    document.getElementById('m').innerHTML = m;
+    document.getElementById('s').innerHTML = s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
